e92d9dcb35eae7259424e97682371ee29dd7352c
David Kerr <dkerr0707@gmail.com>
2026-07-07 16:30:36 -0400

    Basic functionality

--- main.cpp
+++ main.cpp
@@ -21,24 +21,32 @@ public:
 
 int main() {
 
-    std::unordered_map<std::string, Item> items_map = {
+    std::unordered_map<std::string, Item> item_map = {
         { "apple", Item("apple", 25, false) },
         { "bannana", Item("bannana", 25, false) },
         { "oatmeal", Item("oatmeal", 50, false) },
         { "bread", Item("bread", 75, false) }
     };
 
-    auto it = items_map.find("apple");
-    if (it == items_map.end()) { std::cout << "Item not in map" << "\n"; return EXIT_FAILURE; }
+    //auto it = items_map.find("apple");
+    //if (it == items_map.end()) { std::cout << "Item not in map" << "\n"; return EXIT_FAILURE; }
     
-    Item a = it->second;
-    std::cout << a.getSku() << " " << a.getCost() << " " << a.getSpecialPrice() << "\n";
+    //Item a = it->second;
+    //std::cout << a.getSku() << " " << a.getCost() << " " << a.getSpecialPrice() << "\n";
 
     std::vector<std::string> item_list = { "apple", "bannana", "apple", "bread", "oatmeal", "apple" };
-    
+   
+    unsigned int item_sum = 0; 
     for (auto& i: item_list) {
         std::cout << i << "\n";
+        auto it = item_map.find(i);
+        if (it == item_map.end()) { std::cout << "Not in map\n"; return EXIT_FAILURE; }
+        Item a = it->second; 
+        std::cout << a.getSku() << " " << a.getCost() << " " << a.getSpecialPrice() << "\n";
+        item_sum += a.getCost();
     }
+
+    std::cout << "Total : $ " << float(item_sum) / 100 << "\n";
 
     return EXIT_SUCCESS;
 }
